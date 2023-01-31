808c0a898bf84c4b570b5b7e9fa8d7750433f21a
Ju4nM <jenriquemflores@gmail.com>
2023-01-31 00:38:32 -0700

    first commit

---
+++ Calculator_JEMF/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Calculator_JEMF
+{
+    public partial class MainPage : ContentPage
+    {
+        
+        public string Number { get; set; }
+
+        public MainPage()
+        {
+            InitializeComponent();
+            this.Number = "0";
+        }
+
+        public void AddNumber(string Number)
+        {
+            if (this.Number.ElementAt(0) == 0) return;
+
+            this.Number += Number;
+        }
+
+        public void UpdateNumber ()
+        {
+            CurrentNumber.Text = this.Number;
+        }
+    }
+}
